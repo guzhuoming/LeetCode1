6b7aad81febf869ca3f34bdac4d56eba3a62e442
gu <gu@debugging.work>
2020-10-22 09:03:12 +0800

    添加：添加划分字符的题目

--- Project1/Project1/源.cpp
+++ Project1/Project1/源.cpp
@@ -1,5 +1,7 @@
 #include<iostream>
+#include<vector>
 using namespace std;
+// 长按键入 20201021
 bool isLongPressedName(string name, string typed) {
     int i = 0, j = 0;
     bool ans = true;
@@ -46,9 +48,31 @@ bool isLongPressedName(string name, string typed) {
     }
     return ans;
 }
+// 划分字母空间 20201022
+vector<int> partitionLabels(string S) {
+    vector<int> ans;
+    int* last_index = new int[26];
+    for (int i = 0; i < S.length(); i++) {
+        last_index[S[i] - 'a'] = i;
+    }
+    int start = 0, end = 0;
+    for (int i = 0; i < S.length(); i++) {
+        end = max(last_index[S[i] - 'a'], end);
+        if (i == end) {
+            ans.push_back(end - start + 1);
+            start = end + 1;
+        }
+    }
+    return ans;
+}
 int main() {
-    string a = "alex";
-    string b = "aaleex";
-    cout << isLongPressedName(a, b) << endl;
-    return 0;
+    // 长按键入 20201021
+    //string a = "alex";
+    //string b = "aaleex";
+    //cout << isLongPressedName(a, b) << endl;
+    //return 0;
+
+    // 划分字母 20201022
+    string s = "ababcbacadefegdehijhklij";
+    vector<int> ans = partitionLabels(s);
 }
